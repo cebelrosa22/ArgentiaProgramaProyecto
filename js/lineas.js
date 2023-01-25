3af699599eaa8518347bd99d887fbcca5fa77292
Cecilia Rosado <cebelrosa@gmail.com>
2023-01-25 03:26:56 -0300

    correcciones

--- js/lineas.js
+++ js/lineas.js
@@ -8,7 +8,7 @@ function cambiar() {
     const mostrar = (data) => {
         console.log(data)
         document.getElementById("nombres").innerHTML = `${data.results[0].name.first} ${data.results[0].name.last}`
-        document.getElementById("imagen").innerHTML = `<div class="row border-bottom ms-1 me-1 pt-3"></div>
+        document.getElementById("imagen").innerHTML = `<div class="row border-bottom ms-1 me-1 pt-3 mb-2"></div>
         <img id="foto" class="card-img-top ps-3 pe-3 mt-4 mb-4" src="${data.results[0].picture.large}" alt="foto carnet">`
     }
 }
